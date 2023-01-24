ca2d897995edd437a250b3d0255e49c81bc20a0f
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-01-24 14:10:01 +0100

    Everything works without sqlLite

--- src/main/java/hu/fenyvesvolgyimate/tollsystem/dao/SqlLiteVignetteStorage.java
+++ src/main/java/hu/fenyvesvolgyimate/tollsystem/dao/SqlLiteVignetteStorage.java
@@ -18,13 +18,13 @@ public class SqlLiteVignetteStorage implements VignetteStorage {
         vignette.setDateOfPurchase(new Date());
 
         Vignette vignette2 = new Vignette();
-        vignette.setRegistrationNumber("abc-123");
-        vignette.setPrice(321);
-        vignette.setVehicleCategory("m3");
-        vignette.setType("yearly");
-        vignette.setValidFrom(new Date());
-        vignette.setValidTo(new Date());
-        vignette.setDateOfPurchase(new Date());
+        vignette2.setRegistrationNumber("abc-123");
+        vignette2.setPrice(321);
+        vignette2.setVehicleCategory("m3");
+        vignette2.setType("yearly");
+        vignette2.setValidFrom(new Date());
+        vignette2.setValidTo(new Date());
+        vignette2.setDateOfPurchase(new Date());
 
         return List.of(vignette, vignette2);
     }
